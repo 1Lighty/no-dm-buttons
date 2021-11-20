451080815100dac04bdb1cfa53b74f5f11e0ef70
1Lighty <49841131+1Lighty@users.noreply.github.com>
2021-11-20 19:33:57 +0100

    Fix patching something that doesn't exist

--- index.tsx
+++ index.tsx
@@ -4,29 +4,27 @@
 *
 * ————————————————————————————————————————————————————————————————————————————— */
 
-import { getComponent, suppressErrors } from '@util';
-import { after, unpatchAll } from '@patcher';
-import { UPlugin } from '@classes';
+import { before, unpatchAll } from '@patcher';
+import { getByDisplayName } from '@webpack';
+import { suppressErrors } from '@util';
+import { Plugin } from '@classes';
 
-import type { PromisesObject, ReactComponent } from '@classes';
-import type { React } from '@webpack';
-
-export default class NoDMButtons extends UPlugin {
-  _PrivateChannelsList: ReactComponent;
-  start(): void {
-    suppressErrors(this.patchPrivateChannelsList.bind(this))(this.promises);
+export default class NoDMButtons extends Plugin {
+  start() {
+    suppressErrors(this.patchPrivateChannelsList.bind(this))();
   }
-  stop(): void {
+  stop() {
     unpatchAll('no-dm-buttons');
-    if (this._PrivateChannelsList) this._PrivateChannelsList.forceUpdateAll();
   }
-  async patchPrivateChannelsList(promiseState: PromisesObject): Promise<void> {
-    const PrivateChannelsList = await getComponent('PrivateChannelsList', '[id="private-channels"]');
-    if (promiseState.cancelled) return;
-    this._PrivateChannelsList = PrivateChannelsList;
-    after('no-dm-buttons', PrivateChannelsList.component, 'getDerivedStateFromProps', (_, __, ret: { preRenderedChildren: number, nonNullChildren: React.FunctionComponent[] }) => {
-      ret.nonNullChildren = ret.nonNullChildren.filter(e => e && (e.type.toString().indexOf('key:"stage-discovery"') === -1 && e.type.displayName !== 'NitroButton'));
+  patchPrivateChannelsList() {
+    const ConnectedPrivateChannelsList = getByDisplayName('ConnectedPrivateChannelsList', { onlyModule: true });
+    before('no-dm-buttons', ConnectedPrivateChannelsList, 'default', (_, [props]) => {
+      props.showNitroTab = false;
+      props.showLibrary = false;
+      const nitroButtonIdx = props.children.findIndex(e => e?.type?.displayName === 'NitroButton');
+      if (nitroButtonIdx !== -1) props.children.splice(nitroButtonIdx, 1, null);
+      const libraryButtonIdx = props.children.findIndex(e => e?.type?.displayName === 'LibraryButton');
+      if (libraryButtonIdx !== -1) props.children.splice(libraryButtonIdx, 1, null);
     });
-    PrivateChannelsList.forceUpdateAll();
   }
 }
